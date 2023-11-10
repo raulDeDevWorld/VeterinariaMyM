80b874b88dbde3323bb682471c579ecbe9203160
raulDeDevWorld <proyection009@gmail.com>
2023-11-10 18:27:24 -0400

    DC

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#0090A8" />
         <meta name="msapplication-navbutton-color" content="#0090A8" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#0090A8" />
-        <meta name="description" content="Servicios Odotologicos" />
+        <meta name="description" content="Servicios veterianarios y alimento para mascotas" />
         <meta name="keywords" content="Veterinaria MYM" />
         <meta name="author" content="Veterinaria MYM" />
         <title>Veterinaria MYM</title>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBc_WqTHDgHKcJAWnerJ_8Xu-PMOmYUreg",
-  authDomain: "dentalhouse-69348.firebaseapp.com",
-  databaseURL: "https://dentalhouse-69348-default-rtdb.firebaseio.com",
-  projectId: "dentalhouse-69348",
-  storageBucket: "dentalhouse-69348.appspot.com",
-  messagingSenderId: "122721258404",
-  appId: "1:122721258404:web:6d353ccfa166cf335bcf31"
+  apiKey: "AIzaSyDvqcWAhb0PCMoNbXmtpshBCdUmpfsun00",
+  authDomain: "veterinariamym.firebaseapp.com",
+  databaseURL: "https://veterinariamym-default-rtdb.firebaseio.com",
+  projectId: "veterinariamym",
+  storageBucket: "veterinariamym.appspot.com",
+  messagingSenderId: "210726446589",
+  appId: "1:210726446589:web:f606c6e489f96063842594"
 };
 export const app = initializeApp(firebaseConfig);
