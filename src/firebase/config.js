7b0c3594ca5a3297008422f883dc3dd71a865ac5
raulDeDevWorld <proyection009@gmail.com>
2023-11-10 17:58:38 -0400

    DC

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBc_WqTHDgHKcJAWnerJ_8Xu-PMOmYUreg",
+  authDomain: "dentalhouse-69348.firebaseapp.com",
+  databaseURL: "https://dentalhouse-69348-default-rtdb.firebaseio.com",
+  projectId: "dentalhouse-69348",
+  storageBucket: "dentalhouse-69348.appspot.com",
+  messagingSenderId: "122721258404",
+  appId: "1:122721258404:web:6d353ccfa166cf335bcf31"
+};
+export const app = initializeApp(firebaseConfig);
